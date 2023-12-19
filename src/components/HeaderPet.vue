e795d8f6609e71f434e2af570afac29ca20f59d3
FilippoBersan <filippobersan99@gmail.com>
2023-12-19 14:02:42 +0100

    feat: header parte superiore

--- src/components/HeaderPet.vue
+++ src/components/HeaderPet.vue
@@ -1,12 +1,87 @@
 <script>
 export default {
+  name: 'Header Component',
   data() {
-    return {};
+    return {
+      info: [
+        {
+          img: './public/dark-pet-logo.png',
+        },
+      ],
+    };
   },
   components: {},
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <img v-for="info in info" :src="info.img" alt="" />
+    <div class="search">
+      <form>
+        <font-awesome-icon
+          icon="fa-solid fa-magnifying-glass"
+          class="search-icon"
+        />
+        <input type="search" class="search-input" placeholder="Search..." />
+      </form>
+    </div>
+    <div class="questions">
+      <strong>Questions?</strong>
+      <p>Call us : 1.800.123.4567</p>
+    </div>
+    <div class="icons">
+      <font-awesome-icon icon="fa-solid fa-box-archive" />
+      <font-awesome-icon icon="fa-solid fa-user" />
+    </div>
+  </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  justify-content: space-evenly;
+  gap: 20px;
+  height: 40px;
+  border-bottom: 1px solid;
+  border-color: #ececec;
+  padding: 15px;
+  .search {
+    display: flex;
+    align-items: center;
+
+    input::placeholder {
+      position: relative;
+      left: 30px;
+    }
+    .search-icon {
+      position: relative;
+      left: 30px;
+    }
+  }
+
+  .search-input {
+    padding: 10px;
+    width: 300px;
+    border-radius: 20px;
+    background-color: #f4f4f3;
+    border: none;
+  }
+
+  .questions {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    p {
+      color: #7e7e7e;
+    }
+  }
+
+  .icons {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue';
 import './style.scss';
 import App from './App.vue';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-createApp(App).mount('#app');
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import { faBoxArchive } from '@fortawesome/free-solid-svg-icons';
+
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faMagnifyingGlass, faBoxArchive, faUser);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
